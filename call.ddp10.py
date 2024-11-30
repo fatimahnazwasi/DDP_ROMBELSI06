fa717145e1c05d7c10a30e36005fe513504e4c94
fatimahnazwasi <0110124217@student.nurulfikri.ac.id>
2024-11-30 22:42:37 +0700

    Add files via upload

---
+++ call.ddp10.py
@@ -0,0 +1,24 @@
+import main
+
+main.l_persegi(10)
+main.l_persegi_panjang(2, 4)
+main.l_jajargenjang(7, 5)
+main.l_lingkaran(2)
+main.l_segitiga(12, 2)
+
+import bangunruang
+
+bangunruang.l_kubus(12)
+bangunruang.l_balok(2, 4, 10)
+bangunruang.l_prisma(20, 5, 8)
+bangunruang.l_tabung(22/7, 5, 9)
+
+
+import hitung
+hitung.pertambahan(89, 54)
+hitung.pengurangan(200, 5)
+hitung.perkalian(100, 50)
+hitung.pembagian(500, 5)
+hitung.pangkat(2, 3)
+
+
